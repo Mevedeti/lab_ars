055ed72c3b774f3d6c020d0abb6509e6acc65b2d
Mevedeti <127618060+Mevedeti@users.noreply.github.com>
2023-03-21 00:53:04 +0500

    Add files via upload

---
+++ index.php
@@ -0,0 +1,29 @@
+<DOCTYPE html>
+    <html>
+        <head>
+            <meta http-equiv="content-type" content="text/html; charset=windows-1251" />
+            <title>Мир ткани</title>
+            <link href="style.css" rel="stylesheet" type="text/css" />
+</head>
+<body>
+    <table width="800" border="1" align="center" cellpadding="0" cellspacing="0"class="table-border">
+        <tr>
+            <td>
+                <!-- Интернет-магазин "Модный угол", разработчик - Беликов Максим Дмитриевич -->
+                <img src ="images/header.jpg" />
+</td>
+</tr>
+<tr>
+    <td>
+        <?php echo "<p>Тест стиля</p>" ?>
+</td>
+</tr>
+<tr>
+    <td>
+     <!-- подвал сайта: информация о правах на копирование, email:  разработчика - Mevedeti11@gmail.com -->
+     <img src = "images/footer.jpg" />   
+</td>
+</tr>
+</table>
+</body>
+</html>
